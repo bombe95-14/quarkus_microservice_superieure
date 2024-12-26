2f27dfe9567212dda81f0c3835e4983312735964
bombe95-14 <kenfackkeng1995@gmail.com>
2024-12-26 16:55:54 +0100

    debut de la creation des INSCRIPTION

--- quarkus-getting-graph/src/main/java/org/acme/aws/graphql_quarkus/repository/InscriptionRepository.java
+++ quarkus-getting-graph/src/main/java/org/acme/aws/graphql_quarkus/repository/InscriptionRepository.java
@@ -1,17 +1,33 @@
 package org.acme.aws.graphql_quarkus.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 import java.util.List;
+import org.acme.aws.graphql_quarkus.entity.Classroom;
 import org.acme.aws.graphql_quarkus.entity.Eleve;
 import org.acme.aws.graphql_quarkus.entity.Inscription;
+import org.acme.aws.graphql_quarkus.entity.SchoolYear;
+
+
 
 
 
 
 public class InscriptionRepository implements PanacheRepository<Inscription>  {
     
-  public List<Inscription> findByEleve(Eleve eleve){
+public List<Inscription> findByEleve(Eleve eleve){
         return list("eleve", eleve);
+}
+
+public List<Inscription> findByClassRoom( Classroom classroom ){
+
+    return list( "classroom", classroom );
+
+},
+
+
+public List<Inscription> findByEleveAndSchoolYear(Eleve eleve, SchoolYear schoolYear){
+    return list("eleve = :eleve and schoolYear = :schoolYear", Parameters.with("eleve", eleve).and("schoolYear", schoolYear).map());
 }
 
 }
