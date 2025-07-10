13e87b2f8d446f78e03b80028e73adb4277664bd
bombe95-14 <80166051+bombe95-14@users.noreply.github.com>
2025-07-10 10:22:31 +0000

    push of 10/07/25 v1

---
+++ auth_service_spring/auth_spring/src/test/java/auth/service/spring/auth_spring/AuthSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package auth.service.spring.auth_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
